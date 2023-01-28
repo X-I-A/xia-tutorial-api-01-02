32553ba3f3e31337226cee70f8c1589a98d5a157
Soral <cx.soral@gmail.com>
2023-01-28 19:19:49 +0100

    feat: New version

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ from models.purchase_order import PurchaseOrder, Customer
 class Config:
     DEBUG = True
     DEVELOPMENT = True
-    APP_NAME = 'xia-tutorial-api-01'
-    APP_DESCRIPTION = "X-I-A Tutorial API - 01 - Introduction"
+    APP_NAME = 'xia-tutorial-api-01-02'
+    APP_DESCRIPTION = "X-I-A Tutorial API - 01 - 02 - Complex Data Model"
     RESOURCE_MAPPING = {
         "order": PurchaseOrder,
         "customer": Customer
